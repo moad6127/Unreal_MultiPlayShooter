e84db0d8907e7bca3364f006fd4fab0bd977b439
moad6127 <moad6127@gmail.com>
2023-02-11 14:44:21 +0900

    유데미 강좌 : 멀티플레이 플러그인 만들기
    델리게이트와 거기에 필요한 변수들을 선언한후 함수들을 만들고 선언하기

--- MultiplayerGame/MenuSystem/Plugins/MultiplayerSessions/Source/MultiplayerSessions/Private/MultiplayerSessionSubsystem.cpp
+++ MultiplayerGame/MenuSystem/Plugins/MultiplayerSessions/Source/MultiplayerSessions/Private/MultiplayerSessionSubsystem.cpp
@@ -13,3 +13,43 @@ UMultiplayerSessionSubsystem::UMultiplayerSessionSubsystem()
 	}
 
 }
+
+void UMultiplayerSessionSubsystem::CreateSession(int32 NumPublicConnections, FString MatchType)
+{
+}
+
+void UMultiplayerSessionSubsystem::FindSession(int32 MaxSearchResults)
+{
+}
+
+void UMultiplayerSessionSubsystem::JoinSession(const FOnlineSessionSearchResult& SessionResult)
+{
+}
+
+void UMultiplayerSessionSubsystem::DestroySession()
+{
+}
+
+void UMultiplayerSessionSubsystem::StartSession()
+{
+}
+
+void UMultiplayerSessionSubsystem::OnCreateSesionComplete(FName SessionName, bool bWasSuccessful)
+{
+}
+
+void UMultiplayerSessionSubsystem::OnFIndSessionComplete(bool bWasSuccessful)
+{
+}
+
+void UMultiplayerSessionSubsystem::OnJoinSessionComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result)
+{
+}
+
+void UMultiplayerSessionSubsystem::OnDestroySessionComplete(FName SessionName, bool bWasSuccessful)
+{
+}
+
+void UMultiplayerSessionSubsystem::OnStartSessionComplete(FName SessionName, bool bWasSuccessful)
+{
+}
--- MultiplayerGame/MenuSystem/Plugins/MultiplayerSessions/Source/MultiplayerSessions/Public/MultiplayerSessionSubsystem.h
+++ MultiplayerGame/MenuSystem/Plugins/MultiplayerSessions/Source/MultiplayerSessions/Public/MultiplayerSessionSubsystem.h
@@ -18,8 +18,40 @@ class MULTIPLAYERSESSIONS_API UMultiplayerSessionSubsystem : public UGameInstanc
 	
 public:
 	UMultiplayerSessionSubsystem();
+
+	//
+	// to handle session functionality. The Menu class will call these
+	//
+	void CreateSession(int32 NumPublicConnections,FString MatchType);
+	void FindSession(int32 MaxSearchResults);
+	void JoinSession(const FOnlineSessionSearchResult& SessionResult);
+	void DestroySession();
+	void StartSession();
 protected:
+	
+	//
+	//Internal callback for the delegate we'll add to the Online Session interface delegate list
+	// Thise don't need to be called outside this class.
+	//
+
+	void OnCreateSesionComplete(FName SessionName, bool bWasSuccessful);
+	void OnFIndSessionComplete(bool bWasSuccessful);
+	void OnJoinSessionComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result);
+	void OnDestroySessionComplete(FName SessionName, bool bWasSuccessful);
+	void OnStartSessionComplete(FName SessionName, bool bWasSuccessful);
 
 private:
 	IOnlineSessionPtr SessionInterface;
+
+	//
+	// To add to the Online Session Interface delegate list.
+	// We'll bind our MultiplayerSessoinSubsystem Internal callback to these
+	//
+
+	FOnCreateSessionCompleteDelegate CreateSessionCompleteDelegate;
+	FOnFindSessionsCompleteDelegate FindSessionCompleteDelegate;
+	FOnJoinSessionCompleteDelegate JoinSessionCompleteDelegate;
+	FOnDestroySessionCompleteDelegate DestroySEssionCompleteDelegate;
+	FOnStartSessionCompleteDelegate StartSessionCompleteDelegate;
+
 };
