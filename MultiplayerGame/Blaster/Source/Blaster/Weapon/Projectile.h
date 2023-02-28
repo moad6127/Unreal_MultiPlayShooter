b605170b40f681c29a9a5fb42925ac1043b4b38b
moad6127 <moad6127@gmail.com>
2023-02-28 12:58:33 +0900

    유데미 강좌 : 멀티플레이 무기발사
    발사체 HitEvent를 만들기
    OnHit함수를 만들어서 발사체가 다른액터에 맞을경우 사라지게하기
    이때 Destroy함수를 오버라이드 해서 클라이언트와 서버 모두 Impact와 sound을 나타나게 하기

--- MultiplayerGame/Blaster/Source/Blaster/Weapon/Projectile.h
+++ MultiplayerGame/Blaster/Source/Blaster/Weapon/Projectile.h
@@ -15,9 +15,15 @@ public:
 	
 	AProjectile();
 	virtual void Tick(float DeltaTime) override;
+	virtual void Destroyed() override;
+
 protected:
 
 	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	virtual void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+
 
 private:
 	UPROPERTY(EditAnywhere)
@@ -30,6 +36,12 @@ private:
 	class UParticleSystem* Tracer;
 
 	class UParticleSystemComponent* TracerComponent;
+
+	UPROPERTY(EditAnywhere)
+	UParticleSystem* ImpactParticles;
+
+	UPROPERTY(EditAnywhere)
+	class USoundCue* ImpactSound;
 
 public:	
 
