45df3af9f63d603c9294bc451cddc4c04190cda1
moad6127 <moad6127@gmail.com>
2023-03-11 13:51:45 +0900

    유데미 강좌 : 멀티플레이 탄약
    재장전상태일때 무기발사 못하게하기,
    재장전중일때 발사버튼을 누르고있으면 재장전이 끝나고 바로 발사하게 하기

--- MultiplayerGame/Blaster/Source/Blaster/BlasterComponents/CombatComponent.cpp
+++ MultiplayerGame/Blaster/Source/Blaster/BlasterComponents/CombatComponent.cpp
@@ -132,7 +132,7 @@ void UCombatComponent::MulticastFire_Implementation(const FVector_NetQuantize& T
 	{
 		return;
 	}
-	if (Character)
+	if (Character && CombatState == ECombatState::ECS_Unoccupied)
 	{
 		Character->PlayFireMontage(bAiming);
 		EquippedWeapon->Fire(TraceHitTarget);
@@ -201,6 +201,10 @@ void UCombatComponent::FinishReload()
 	{
 		CombatState = ECombatState::ECS_Unoccupied;
 	}
+	if (bFireButtonPressed)
+	{
+		Fire();
+	}
 }
 
 void UCombatComponent::OnRep_CombatState()
@@ -209,6 +213,12 @@ void UCombatComponent::OnRep_CombatState()
 	{
 	case ECombatState::ECS_Reloading:
 		HandleReload();
+		break;
+	case ECombatState::ECS_Unoccupied:
+		if (bFireButtonPressed)
+		{
+			Fire();
+		}
 		break;
 	}
 }
@@ -407,7 +417,7 @@ bool UCombatComponent::CanFire()
 	{
 		return false;
 	}
-	return !EquippedWeapon->IsEmpty() || !bCanFire;
+	return !EquippedWeapon->IsEmpty() && bCanFire && CombatState == ECombatState::ECS_Unoccupied;
 }
 
 void UCombatComponent::OnRep_CarriedAmmo()
