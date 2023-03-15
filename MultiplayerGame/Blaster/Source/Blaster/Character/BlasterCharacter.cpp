fccd4fc3609dded931353f9b855587f5a3bbaa56
moad6127 <moad6127@gmail.com>
2023-03-15 15:46:38 +0900

    유데미 강좌 : 멀티플레이 여러가지 무기타입
    RocketMovementComponent를 만들어서 로켓이 자기자신을 맞추더라도 계속 진행하도록 만들기
    ,
    WeaponMovement를 복제해서 서버와 클라이언트가 모두 같은위치의 무기를 볼수있게 하기

--- MultiplayerGame/Blaster/Source/Blaster/Character/BlasterCharacter.cpp
+++ MultiplayerGame/Blaster/Source/Blaster/Character/BlasterCharacter.cpp
@@ -295,6 +295,9 @@ void ABlasterCharacter::PlayReloadMontage()
 		case EWeaponType::EWT_AssaultRifle:
 			SectioinName = FName("Rifle");
 			break;
+		case EWeaponType::EWT_RocketLauncher:
+			SectioinName = FName("Rifle");
+			break;
 		}
 		AnimInstance->Montage_JumpToSection(SectioinName);
 	}
