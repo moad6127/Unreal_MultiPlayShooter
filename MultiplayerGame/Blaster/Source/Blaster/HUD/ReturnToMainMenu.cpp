1b6de1821e5932c025869ff148a2669160bfb5ee
moad6127 <moad6127@gmail.com>
2023-04-07 11:59:25 +0900

    유데미 강좌 : 멀티플레이 멀티플레이 기능들
    인게임에서 메인메뉴로 나가기위해 위젯과 C++파일 만들기

---
+++ MultiplayerGame/Blaster/Source/Blaster/HUD/ReturnToMainMenu.cpp
@@ -0,0 +1,105 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ReturnToMainMenu.h"
+#include "GameFramework/PlayerController.h"
+#include "Components/Button.h"
+#include "MultiplayerSessionSubsystem.h"
+#include "GameFramework/GameModeBase.h"
+
+void UReturnToMainMenu::MenuSetup()
+{
+	AddToViewport();
+	SetVisibility(ESlateVisibility::Visible);
+	bIsFocusable = true;
+
+	UWorld* World = GetWorld();
+	if (World)
+	{
+		PlayerController = PlayerController == nullptr ? World->GetFirstPlayerController() : PlayerController;
+		if (PlayerController)
+		{
+			FInputModeGameAndUI InputModeDate;
+			InputModeDate.SetWidgetToFocus(TakeWidget());
+			PlayerController->SetInputMode(InputModeDate);
+			PlayerController->SetShowMouseCursor(true);
+		}
+	}
+	if (ReturnButton)
+	{
+		ReturnButton->OnClicked.AddDynamic(this, &UReturnToMainMenu::ReturnButtonClicked);
+	}
+	UGameInstance* GameInstance = GetGameInstance();
+	if (GameInstance)
+	{
+		MultiplayerSessionSubsystem = GameInstance->GetSubsystem<UMultiplayerSessionSubsystem>();
+		if (MultiplayerSessionSubsystem)
+		{
+			MultiplayerSessionSubsystem->MultiplayerOnDestroySessionComplete.AddDynamic(this, &UReturnToMainMenu::OnDestroySession);
+		}
+	}
+}
+
+bool UReturnToMainMenu::Initialize()
+{
+	if (!Super::Initialize())
+	{
+		return false;
+	}
+
+	return true;
+}
+
+void UReturnToMainMenu::OnDestroySession(bool bWasSuccessful)
+{
+	if (!bWasSuccessful)
+	{
+		ReturnButton->SetIsEnabled(true);
+	}
+
+	UWorld* World = GetWorld();
+	if (World)
+	{
+		AGameModeBase* GameMode = World->GetAuthGameMode<AGameModeBase>();
+		if (GameMode)
+		{
+			GameMode->ReturnToMainMenuHost();
+		}
+		else
+		{
+			PlayerController = PlayerController == nullptr ? World->GetFirstPlayerController() : PlayerController;
+			if (PlayerController)
+			{
+				PlayerController->ClientReturnToMainMenuWithTextReason(FText());
+			}
+		}
+	}
+}
+
+void UReturnToMainMenu::MenuTearDown()
+{
+	RemoveFromParent();
+
+	UWorld* World = GetWorld();
+	if (World)
+	{
+		PlayerController = PlayerController == nullptr ? World->GetFirstPlayerController() : PlayerController;
+		if (PlayerController)
+		{
+			FInputModeGameOnly InputModeDate;
+
+			PlayerController->SetInputMode(InputModeDate);
+			PlayerController->SetShowMouseCursor(false);
+		}
+	}
+}
+
+void UReturnToMainMenu::ReturnButtonClicked()
+{
+	ReturnButton->SetIsEnabled(false);
+
+	if (MultiplayerSessionSubsystem)
+	{
+		MultiplayerSessionSubsystem->DestroySession();
+	}
+}
